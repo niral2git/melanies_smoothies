690e19ae4174f650b853197ddfe7f64e65bf1595
niral2git <niralhmehta@gmail.com>
2026-08-18 18:03:34 +0530

    Fix smoothie nutrition information display
    Updated the response handling for smoothie nutrition information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,5 @@ if ingredients_list:
 
 #New section to display smoothie nutrition information
 import requests
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")
-st.text(smoothiefroot_response.json())
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
